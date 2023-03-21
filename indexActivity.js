b01497022c1c2dad7bdc469d7b5a51c2364e2fd2
Ral Vjince Lomarda <ral.lomarda@gmail.com>
2023-03-21 12:33:02 +0800

    adding main activity

---
+++ indexActivity.js
@@ -0,0 +1,23 @@
+console.log("Hello, Welcome to De La Salle University!")
+
+let firstName = 'Ral Vjince'
+let lastName = 'Lomarda'
+let age = 22
+let hobbies = ["Movies/T.V Shows", "Anime", "Kalimba",
+                "Video Games", "Learn stuff"]
+let homeAddress = {
+    houseNumber: '051',
+    street: "Kalye Masikap",
+    subdivision: "N/A",
+    barangay: "Tanzang Luma II",
+    city: "Imus",
+    province: "Cavite",
+    zipCode: 4103
+    }
+
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(hobbies)
+console.log(homeAddress)
